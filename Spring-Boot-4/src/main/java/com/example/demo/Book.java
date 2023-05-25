fe6e2647b17f5df4aa2a1fda19a5f2be8a7947aa
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2023-05-25 12:53:57 +0530

    Add files via upload

---
+++ Spring-Boot-4/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String isbn;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + "]";
+	}
+	public Book(String title, String isbn) {
+		super();
+		//this.id = id;
+		this.title = title;
+		this.isbn = isbn;
+	}
+	
+	
+	public Book() {
+		
+	}
+}
---
+++ Spring-Boot-4/src/main/java/com/example/demo/SpringBoot4Application.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4Application implements CommandLineRunner{
+
+	@Autowired
+	BookRepository bookRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		bookRepository.save(new Book("Thinking in Java","483748"));
+		bookRepository.save(new Book("Java 9 to 17","45738"));
+		bookRepository.save(new Book("Java Interview prep","857385"));
+		System.out.println("Books added successfully ");
+		
+		for(Book book:bookRepository.findAll())
+			System.out.println(book);
+	}
+
+}
